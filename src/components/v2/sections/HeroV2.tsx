7f6efb9914b1b9e5c95bafab20ea3b9dc0939c33
Juan Miguel <juanmiguelriveroyhornos@gmail.com>
2026-04-07 00:26:03 -0300

    Monday

--- src/components/v2/sections/HeroV2.tsx
+++ src/components/v2/sections/HeroV2.tsx
@@ -21,7 +21,7 @@ export const HeroV2 = () => {
           alt=""
           fill
           priority
-          className="object-cover object-center opacity-20"
+          className="object-cover object-center opacity-[0.56]"
         />
       </div>
 
